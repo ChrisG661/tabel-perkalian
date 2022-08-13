fd0c95a7fd8d74f7ddc8999b2a6481753ed74af4
Christopher G <59037609+ChrisG661@users.noreply.github.com>
2022-08-13 11:55:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 var table = document.getElementById("tabelPerkalian");
 
 function tabelPerkalian(table, sizeCol, sizeRow) {
-    if (!sizeCol || !sizeRow) {
-        sizeCol = 10;
-        sizeRow = 10;
-    }
+    sizeRow = !sizeRow ? 10 : sizeRow
+    sizeCol = !sizeCol ? 10 : sizeCol
+
+    if (sizeCol < 1 || sizeRow < 1) return;
 
     let tableHead = table.querySelector("thead tr")
     let tableBody = table.getElementsByTagName("tbody")[0]
     tableHead.innerHTML = ""
     tableBody.innerHTML = ""
     tableHead.innerHTML += `<th scope='col'>&times;</th>`
+
     for (var col = 1; col <= sizeCol; col++) {
         // append header
         tableHead.innerHTML += `<th class="py-2" scope='col'>${col}</th>`
+    }
+
+    for (var row = 1; row <= sizeRow; row++) {
+        // append side header
         let tr = document.createElement("tr");
         tr.className = "bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-        tr.innerHTML += `<th class="px-2 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400" cscope='row'>${col}</th>`
-        for (var row = 1; row <= sizeRow; row++) {
+        tr.innerHTML += `<th class="px-2 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400" cscope='row'>${row}</th>`
+        for (var col = 1; col <= sizeCol; col++) {
             // append row
             tr.innerHTML += `<td data-col=${col} data-row=${row} class="py-2 px-3 hover:bg-gray-50">${col * row}</td>`;
         }
         tableBody.appendChild(tr);
     }
-
 }
 
 function update(e) {
@@ -35,7 +39,7 @@ function update(e) {
 
 var sizeInput = document.querySelectorAll(".ukuran input")
 
-tabelPerkalian(table, 10, 10)
+tabelPerkalian(table)
 
 table.addEventListener('mousemove', e => {
     let onCell = document.elementFromPoint(e.clientX, e.clientY)
@@ -44,6 +48,6 @@ table.addEventListener('mousemove', e => {
 }, { passive: true })
 
 document.querySelectorAll(".ukuran input").forEach((e) => {
-    e.onchange = update;
+    e.oninput = update;
 });
 
